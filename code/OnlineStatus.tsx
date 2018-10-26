4fc3a5c7e032040a9140516d8a1f8fd286606fba
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2018-10-26 18:29:23 +0800

    update code

--- code/OnlineStatus.tsx
+++ code/OnlineStatus.tsx
@@ -1,38 +1,70 @@
-import * as React from "react";
-import { PropertyControls, ControlType } from "framer";
-
-import useOnlineStatus from '@rehooks/online-status';
-
-const style: React.CSSProperties = {
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    color: "#8855FF",
-    background: "rgba(136, 85, 255, 0.1)",
-    overflow: "hidden",
-};
+import * as React from 'react';
+import { PropertyControls, ControlType } from 'framer';
 
 // Define type of property
-interface Props {
-    text: string;
-}
+interface Props {}
 
 export class OnlineStatus extends React.Component<Props> {
+  // Set default properties
+  static defaultProps = {};
 
-    // Set default properties
-    static defaultProps = {
-    text: "Hello World!"
-    }
+  getOnlineStatus() {
+    return typeof navigator !== 'undefined' && typeof navigator.onLine === 'boolean' ? navigator.onLine : true;
+  }
 
-    // Items shown in property panel
-    static propertyControls: PropertyControls = {
-    text: { type: ControlType.String, title: "Text" },
-    }
+  state = {
+    online: this.getOnlineStatus(),
+  };
 
-    render() {
-        let onlineStatus = useOnlineStatus();
-    return <div style={style}>{onlineStatus ? 'online' : 'offline'}</div>;
+  updateOnlineStatus() {
+    this.setState({
+      online: this.getOnlineStatus(),
+    });
+  }
+
+  componentDidMount() {
+    console.log(navigator);
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.updateOnlineStatus.bind(this));
+    window.removeEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  // Items shown in property panel
+  static propertyControls: PropertyControls = {
+    children: {
+      type: ControlType.Array,
+      title: 'Content',
+      propertyControl: {
+        type: ControlType.ComponentInstance,
+        title: 'Page',
+      },
+    },
+  };
+
+  render() {
+    if (this.props.children.length == 0) {
+      return (
+        <div
+          style={{
+            border: '1px dashed #ddd',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1,
+            height: '100%',
+            color: '#ddd',
+            textAlign: 'center',
+            padding: 16,
+          }}
+        >
+          Connect to a frame to be online state
+        </div>
+      );
     }
+    return <div>{this.state.online ? this.props.children[0] : this.props.children[1]}</div>;
+  }
 }
